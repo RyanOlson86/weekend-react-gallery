b0ebf6c19924c96f991c619c4ab386c378e40f5f
Ryan Olson <ryan.olson86@yahoo.com>
2024-01-14 13:41:37 -0600

    updated database.sql

--- database.sql
+++ database.sql
@@ -9,5 +9,7 @@ CREATE TABLE "gallery" (
 INSERT INTO "gallery" 
 ("url", "title", "description")
 VALUES
+('images/squirrel.jpg', 'Squirrel!', 'Photo of a squirrel cracking a nut.'),
 ('images/goat_small.jpg', 'Goat!', 'Photo of a goat taken at Glacier National Park.');
-  
+
+UPDATE "gallery" SET likes= likes + 1 WHERE id=1;
